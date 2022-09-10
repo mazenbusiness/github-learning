117ab172a0968d559556a5a425a57ba33da1e362
unknown <mazen.business1991@gmail.com>
2022-09-10 23:05:04 +0300

    update to the branch

--- src/component/jar.jsx
+++ src/component/jar.jsx
@@ -1,5 +1,10 @@
+import { Fragment } from "react";
+
 const Jar = () => {
-  return <h1>hi</h1>;
+  return;
+  <Fragment>
+    <h1>hi</h1>;<p> adding a new branch</p>
+  </Fragment>;
 };
 
 export default Jar;
